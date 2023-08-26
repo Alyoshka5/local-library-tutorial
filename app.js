942af1650711c2f227c1f8b96f46cb1845507bbd
Alyoshka <alyoshkamyshko@gmail.com>
2023-08-25 21:29:03 -0700

    Change env variable for mongo db uri

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 // database setup
 mongoose.set('strictQuery', false);
 const dev_db_url = "mongodb+srv://alyoshkamyshko:myatlas-000@cluster0.6uhgpay.mongodb.net/local_library?retryWrites=true&w=majority";
-const mongoDB = process.env.mongoDB_URI || dev_db_url;
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch((err) => console.log(err));
 async function main() {
