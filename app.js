a7789d3b2b7ebee8878b314f81be26513aac86a6
Alyoshka <alyoshkamyshko@gmail.com>
2023-08-25 20:42:42 -0700

    Create project

--- app.js
+++ app.js
@@ -3,24 +3,55 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require("mongoose");
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const catalogRouter = require("./routes/catalog");
+const compression = require('compression');
+const helmet = require('helmet');
+const RateLimit = require('express-rate-limit');
 
 const app = express();
+
+// database setup
+mongoose.set('strictQuery', false);
+const dev_db_url = "mongodb+srv://alyoshkamyshko:myatlas-000@cluster0.6uhgpay.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.mongoDB_URI || dev_db_url;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
+app.use(limiter);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ models/author.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+    first_name: { type: String, required: true, maxLength: 100 },
+    family_name: { type: String, required: true, maxLength: 100 },
+    date_of_birth: { type: Date },
+    date_of_death: { type: Date },
+});
+
+AuthorSchema.virtual('name').get(function() {
+    let fullname = "";
+    if (this.first_name && this.family_name) {
+        fullname = `${this.first_name} ${this.family_name}`;
+    }
+
+    return fullname;
+});
+
+AuthorSchema.virtual('url').get(function() {
+    return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual('lifespan').get(function() {
+    const DOB = this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+    const DOD = this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+    return `${DOB} - ${DOD}`;
+});
+
+AuthorSchema.virtual("date_of_birth_input_format").get(function () {
+    return DateTime.fromJSDate(this.date_of_birth).toISODate(); // format 'YYYY-MM-DD'
+});
+
+AuthorSchema.virtual("date_of_death_input_format").get(function () {
+    return DateTime.fromJSDate(this.date_of_death).toISODate(); // format 'YYYY-MM-DD'
+});
+
+module.exports = mongoose.model('Author', AuthorSchema);
